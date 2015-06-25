f0843013c58738283df63970246f493be0b3a4d2
1101137105 <haozhen1106>
2015-06-26 04:16:26 +0800

    增加單元測試

---
+++ KuasCoreTests/Dao/MemberDaoUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using KuasCore.Dao;
+using KuasCore.Models;
+using KuasCore.Services.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Testing.Microsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCoreTests.Dao
+{
+    [TestClass]
+   public class MemberDaoUnitTest : AbstractDependencyInjectionSpringContextTests
+    {
+        #region Spring 單元測試必寫的內容
+
+        override protected string[] ConfigLocations
+        {
+            get
+            {
+                return new String[] { 
+                    "~/Config/KuasCoreDatabase.xml",
+                    "~/Config/KuasCorePointcut.xml",
+                    "~/Config/KuasCoreTests.xml" 
+                };
+            }
+        }
+
+        #endregion
+
+        public IMemberDao MemberDao { get; set; }
+
+
+        [TestMethod]
+        public void TestMemberDao_AddMember()
+        {
+            Member member = new Member();
+
+            member.name = "haotest";
+            member.email = "haotest@gmail.com";
+            member.password = "test";
+            member.account = "test";
+            member.sex = "boy";
+            member.age = 21;
+            MemberDao.AddMember(member);
+
+            Member dbMember = MemberDao.GetMemberByName(member.name);
+            Assert.IsNotNull(dbMember);
+            Assert.AreEqual(member.name, dbMember.name);
+
+            Console.WriteLine("會員性別為 = " + dbMember.sex);
+            Console.WriteLine("會員名稱為 = " + dbMember.name);
+            Console.WriteLine("會員信箱為 = " + dbMember.email);
+
+            MemberDao.DeleteMember(dbMember);
+            dbMember = MemberDao.GetMemberByName(member.name);
+            Assert.IsNull(dbMember);
+        }
+    }
+}
---
+++ KuasCoreTests/Services/Impl/MemberServiceUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Testing.Microsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCoreTests.Services.Impl
+{
+    [TestClass]
+    class MemberServiceUnitTest : AbstractDependencyInjectionSpringContextTests
+    {
+        override protected string[] ConfigLocations
+        {
+            get
+            {
+                return new String[] { 
+                    //assembly://MyAssembly/MyNamespace/ApplicationContext.xml
+                    "~/Config/KuasCoreDatabase.xml",
+                    "~/Config/KuasCorePointcut.xml",
+                    "~/Config/KuasCoreTests.xml" 
+                };
+            }
+        }
+        public IMemberService MemberService { get; set; }
+        [TestMethod]
+        public void TestMemberService_AddMember()
+        {
+
+            Member member = new Member();
+
+            member.name = "haotest";
+            member.email = "haotest@gmail.com";
+            member.password = "test";
+            member.account = "test";
+            member.sex = "boy";
+            member.age = 21;
+            MemberService.AddMember(member);
+
+            Member dbMember = MemberService.GetMemberByName(member.name);
+            Assert.IsNotNull(dbMember);
+            Assert.AreEqual(member.name, dbMember.name);
+
+            Console.WriteLine("會員性別為 = " + dbMember.sex);
+            Console.WriteLine("會員名稱為 = " + dbMember.name);
+            Console.WriteLine("會員信箱為 = " + dbMember.email);
+
+            MemberService.DeleteMember(dbMember);
+            dbMember = MemberService.GetMemberByName(member.name);
+            Assert.IsNull(dbMember);
+        }
+    }
+}
